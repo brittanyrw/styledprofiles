cc9a5e692b012a7a8f1d8819a15a1af83c7c47b2
Brittany Walker <breneewalker@gmail.com>
2019-08-14 14:58:23 -0400

    set up initial grid with example profiles

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var profileContent = '';
+
+var counter = 1;
+
+for(var i = 0; i < 23; i++){
+    profileContent += '<div class="profile"><header><h1>Brittany Walker' + counter + '</h1><h2>Software Engineer</h2><p class="location">Washington, DC</p><p>Favorite Emoji: 💖</p></header><div class="profile-content"><div class="interests"><ul><li>CSS</li><li>HTML</li><li>Vue.js</li><li>Open Source</li></ul></div><p class="profile-bio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quia veritatis! Facilis obcaecati illo magni dicta sint voluptatem aliquid, mollitia, soluta harum, optio quod praesentium perspiciatis sapiente deleniti. Quibusdam, excepturi!</p><div class="contact"><a href="">musicalwebdev.com</a></div></div></div>';
+    counter++;
+}
+
+
+$('#profiles').append(profileContent);
