5bec5ce93b4cdeae63c06d82f4effc7629f36186
Brittany Walker <breneewalker@gmail.com>
2019-08-21 15:24:23 -0400

    remove json and ajax, add github profile to test data

---
+++ data.js
@@ -0,0 +1,12 @@
+const profiles = [
+    {
+        name: "Brittany Walker",
+        occupation: "Software Engineer",
+        location: "WASHINGTON",
+        emoji: "💖",
+        interests: ["CSS", "Vue.js", "Teaching", "Open Source"],
+        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quia veritatis! Facilis obcaecati illo magni dicta sint voluptatem aliquid, mollitia, soluta harum, optio quod praesentium perspiciatis sapiente deleniti. Quibusdam, excepturi!",
+        github: 'brittanyrw',
+        link: "https://musicalwebdev.com"
+    }
+]
